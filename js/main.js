b6c5a6801bcfe767db2507409c577f79118f8841
Jem <jerameabalo51@gmail.com>
2026-08-25 19:00:21 +0800

    Change title from 'Quim' to 'Maxinne'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('For You, Quim').split('')
+    const titles = ('For You, Maxinne').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
