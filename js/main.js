3fadf15b60402fba24803f1b4b7749b9417c8754
Jem <jerameabalo51@gmail.com>
2026-08-30 20:10:41 +0800

    Change title from 'Maxinne' to 'Baby'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('For You, Maxinne').split('')
+    const titles = ('For You, Baby').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
